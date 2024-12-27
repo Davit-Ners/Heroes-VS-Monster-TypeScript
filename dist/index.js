5d54aae33c24fd1283783acc80a2d20f235e013a
Nersesyan <davit.nersesyan@student.vinci.be>
2024-12-27 15:36:42 +0100

    Creation de toutes les classes

--- dist/index.js
+++ dist/index.js
@@ -1,203 +1,147 @@
+"use strict";
 // Création des personnages du jeu
-
 class De {
-
-    private _nbFaces: number;
-
-    constructor(nbFaces: number) {
+    _nbFaces;
+    constructor(nbFaces) {
         this._nbFaces = nbFaces;
     }
-
-    
-    public get nbFaces() : number {
+    get nbFaces() {
         return this._nbFaces;
     }
-    
-    public lancer(): number {
+    lancer() {
         return Math.ceil(Math.random() * this._nbFaces);
     }
-
 }
-
 class Personnage {
-
-    protected _end: number;
-    protected _force: number;
-    protected _pv: number;
-
+    _end;
+    _force;
+    _pv;
     constructor() {
-
         this._end = this.calculerStates();
-
         this._force = this.calculerStates();
-
-        const modifier: number = this.calculateModifier(this._end);
+        const modifier = this.calculateModifier(this._end);
         this._pv = this._end + modifier;
-
     }
-
     // Getters
-    
-    public get end(): number {
+    get end() {
         return this._end;
     }
-
-    public get force(): number {
+    get force() {
         return this._force;
-    }   
-
-    public get pv(): number {
+    }
+    get pv() {
         return this._pv;
     }
-
-    public get maxPv(): number {
+    get maxPv() {
         return this.calculateModifier(this._end) + this._end;
     }
-
-    public get isAlive(): boolean {
+    get isAlive() {
         return this._pv > 0;
     }
-
     // Setters
-
-    public set pv(value: number) {
+    set pv(value) {
         this._pv = value;
     }
-    
-    
-    
-    public attaque(personnage: Personnage) {
-        const de: De = new De(4);
-        const modifier: number = this.calculateModifier(this.force);
+    attaque(personnage) {
+        const de = new De(4);
+        const modifier = this.calculateModifier(this.force);
         const degat = de.lancer() + modifier;
-
         personnage._pv -= degat;
     }
-
-    private calculerStates(): number {
-
-        let tabRand: number[] = [];
-        const de: De = new De(6);
-
+    calculerStates() {
+        let tabRand = [];
+        const de = new De(6);
         for (let i = 0; i < 4; i++) {
-            const ran: number = de.lancer();
+            const ran = de.lancer();
             tabRand.push(ran);
         }
-
         tabRand = tabRand.sort((a, b) => b - a);
-
         return tabRand[0] + tabRand[1] + tabRand[2];
-
     }
-
-    private calculateModifier(stat: number): number {
-        if (stat < 5) return -1;
-        if (stat < 10) return 0;
-        if (stat < 15) return 1;
+    calculateModifier(stat) {
+        if (stat < 5)
+            return -1;
+        if (stat < 10)
+            return 0;
+        if (stat < 15)
+            return 1;
         return 2;
     }
-
 }
-
 class Hero extends Personnage {
-
-    private _or: number;
-    private _cuir: number;
-
+    _or;
+    _cuir;
     constructor() {
         super();
         this._or = 0;
         this._cuir = 0;
     }
-
-    public get or(): number {
+    get or() {
         return this._or;
     }
-
-    public get cuir(): number {
+    get cuir() {
         return this._cuir;
     }
-
-    public loot(monstre: Monstre): void {
-        if (monstre.or) this._or += monstre.or
-        if (monstre.cuir) this._cuir += monstre.cuir;
+    loot(monstre) {
+        if (monstre.or)
+            this._or += monstre.or;
+        if (monstre.cuir)
+            this._cuir += monstre.cuir;
     }
-
-    public repos(): void {
+    repos() {
         this.pv = this.maxPv;
     }
-
 }
-
 class Human extends Hero {
-    get end(): number {
+    get end() {
         return this._end + 1;
     }
-
-    get force(): number {
+    get force() {
         return this._force + 1;
     }
 }
-
 class Nain extends Hero {
-    get end(): number {
+    get end() {
         return this._end + 2;
     }
 }
-
 class Monstre extends Personnage {
-
-    protected _or: number;
-    protected _cuir: number;
-
+    _or;
+    _cuir;
     constructor() {
         super();
         this._or = 0;
         this._cuir = 0;
     }
-
-    public get or(): number {
+    get or() {
         return this._or;
     }
-
-    public get cuir(): number {
+    get cuir() {
         return this._cuir;
     }
-
 }
-
 class Loup extends Monstre {
-
     constructor() {
         super();
         this._cuir = 1;
     }
-
 }
-
 class Orc extends Monstre {
-
     constructor() {
         super();
         this._or = 1;
     }
-
-    get force(): number {
+    get force() {
         return this._force + 1;
     }
-
 }
-
 class Dragonnet extends Monstre {
-
     constructor() {
         super();
         this._or = 1;
         this._cuir = 1;
     }
-
-    get end(): number {
+    get end() {
         return this._end + 1;
     }
-
 }
